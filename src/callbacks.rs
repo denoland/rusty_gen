473641ed9ee7d874189158a179790441a87bf3c1
Bert Belder <bertbelder@gmail.com>
2020-07-09 12:43:23 +0200

    wip

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -8,6 +8,7 @@ use std::collections::HashSet;
 use std::fmt::{self, Display, Formatter};
 use std::hash::Hash;
 use std::hash::Hasher;
+use std::iter::once;
 use std::marker::PhantomData;
 use std::mem::transmute;
 use std::ops::{Deref, DerefMut};
@@ -380,11 +381,23 @@ enum TypeParams<'tu> {
   Two(Type<'tu>, Type<'tu>),
 }
 
-fn is_v8_type<'tu>(ty: Type<'tu>, name: &str) -> Option<TypeParams<'tu>> {
+fn is_v8_type<'tu>(ty: Type<'tu>, name: &str) -> Option<Type<'tu>> {
   let _def = ty
     .get_declaration()
     .filter(|d| d.get_name().map(|n| n == name).unwrap_or(false))
     .filter(|d| d.get_semantic_parent().map(is_v8_ns).unwrap_or(false))?;
+  Some(ty)
+}
+
+fn is_v8_type_pointee<'tu>(ty: Type<'tu>, name: &str) -> Option<Type<'tu>> {
+  ty.get_pointee_type().and_then(|ty| is_v8_type(ty, name))
+}
+
+fn get_type_name<'tu>(ty: Type<'tu>) -> Option<String> {
+  ty.get_declaration().and_then(|d| d.get_name())
+}
+
+fn get_type_params<'tu>(ty: Type<'tu>) -> Option<TypeParams<'tu>> {
   let mut ptys = ty.get_template_argument_types()?.into_iter();
   let pty1 = ptys.next();
   let pty2 = pty1.and_then(|_| ptys.next());
@@ -393,9 +406,7 @@ fn is_v8_type<'tu>(ty: Type<'tu>, name: &str) -> Option<TypeParams<'tu>> {
     (None, _, _) => TypeParams::Zero,
     (Some(p), None, _) => TypeParams::One(p?),
     (Some(p), Some(q), None) => TypeParams::Two(p?, q?),
-    _ => {
-      return None;
-    }
+    _ => panic!("More than 2 type parameters"),
   };
   Some(info)
 }
@@ -423,64 +434,121 @@ fn is_std_type<'tu>(ty: Type<'tu>, name: &str) -> bool {
 enum TypeMapping<'tu> {
   Void,
   Bool,
-  IntN(u8),
-  UIntN(u8),
-  UIntSize,
+  Int { signed: bool },
+  IntBits { bits: u8, signed: bool },
+  IntSize { signed: bool },
   Local(String),
   MaybeLocal(String),
+  Isolate { mutable: bool },
+  CallbackInfo { name: String, ret_ty_name: String },
   Unknown(Type<'tu>),
+}
+
+#[derive(Debug)]
+enum Identifier {
+  Return,
+  Anonymous,
+  Param(String),
+}
+
+impl From<Option<String>> for Identifier {
+  fn from(s: Option<String>) -> Identifier {
+    s.map(Identifier::Param)
+      .unwrap_or_else(|| Identifier::Anonymous)
+  }
 }
 
 fn visit_type<'tu>(
   defs: &'_ mut ClassDefIndex<'tu>,
   ty: Type<'tu>,
-) -> Option<()> {
+  id: Identifier,
+) -> Option<(Identifier, TypeMapping<'tu>)> {
   use TypeKind as K;
   use TypeMapping as M;
 
-  let mapping = if ty.get_kind() == K::Void {
+  let ty_map = if ty.get_kind() == K::Void {
     M::Void
   } else if ty.get_kind() == K::Bool {
     M::Bool
+  } else if ty.get_kind() == K::Int {
+    M::Int { signed: true }
   } else if is_std_type(ty, "int32_t") {
-    M::IntN(32)
+    M::IntBits {
+      bits: 32,
+      signed: true,
+    }
   } else if is_std_type(ty, "int64_t") {
-    M::IntN(64)
+    M::IntBits {
+      bits: 64,
+      signed: true,
+    }
   } else if is_std_type(ty, "uint32_t") {
-    M::UIntN(32)
+    M::IntBits {
+      bits: 32,
+      signed: false,
+    }
   } else if is_std_type(ty, "uint64_t") {
-    M::UIntN(64)
+    M::IntBits {
+      bits: 64,
+      signed: false,
+    }
+  } else if is_std_type(ty, "ssize_t") || is_std_type(ty, "intptr_t") {
+    M::IntSize { signed: true }
   } else if is_std_type(ty, "size_t") || is_std_type(ty, "uintptr_t") {
-    M::UIntSize
-  } else if let Some(TypeParams::One(p)) = is_v8_type(ty, "Local") {
-    M::Local(p.get_display_name())
-  } else if let Some(TypeParams::One(p)) = is_v8_type(ty, "MaybeLocal") {
-    M::MaybeLocal(p.get_display_name())
+    M::IntSize { signed: false }
+  } else if let Some(TypeParams::One(p)) =
+    is_v8_type(ty, "Local").and_then(get_type_params)
+  {
+    M::Local(get_type_name(p).unwrap_or_else(|| "<ERR>".to_owned()))
+  } else if let Some(TypeParams::One(p)) =
+    is_v8_type(ty, "MaybeLocal").and_then(get_type_params)
+  {
+    M::MaybeLocal(get_type_name(p).unwrap_or_else(|| "<ERR>".to_owned()))
+  } else if let Some(pty) = is_v8_type_pointee(ty, "Isolate") {
+    M::Isolate {
+      mutable: !pty.is_const_qualified(),
+    }
+  } else if let Some(ty2) = is_v8_type_pointee(ty, "FunctionCallbackInfo")
+    .or_else(|| is_v8_type_pointee(ty, "PropertyCallbackInfo"))
+  {
+    let name = get_type_name(ty2).unwrap_or_else(|| "<ERR>".to_owned());
+    let ret_ty_name = get_type_params(ty2)
+      .and_then(|ps| match ps {
+        TypeParams::Zero => None,
+        TypeParams::One(p) => Some(p),
+        _ => panic!(),
+      })
+      .and_then(get_type_name)
+      .unwrap_or_else(|| "<ERR>".to_owned());
+    M::CallbackInfo { name, ret_ty_name }
   } else {
-    M::Unknown(ty)
+    //M::Unknown(ty)
+    return None;
   };
-  println!("T: {:?}", mapping);
-  Some(())
+  Some((id, ty_map))
 }
 
 fn visit_callback_definition<'tu>(
   defs: &'_ mut ClassDefIndex<'tu>,
   e: Entity<'tu>,   // The typedef definition node.
   fn_ty: Type<'tu>, // The callback function prototype.
-) -> Option<()> {
-  println!(
-    "{:<50} => {}",
-    get_flat_name_for_callback_type(e),
-    fn_ty.get_display_name()
-  );
+) -> Option<Vec<(Identifier, TypeMapping<'tu>)>> {
   let ret_ty = fn_ty.get_result_type()?;
   let arg_tys = fn_ty.get_argument_types()?;
-  let arg_names = e.get_children();
-  visit_type(defs, ret_ty).unwrap();
-  arg_tys
+  let arg_names = e
+    .get_children()
     .into_iter()
-    .for_each(|a| visit_type(defs, a).unwrap());
-  Some(())
+    .filter(|c| c.get_kind() == EntityKind::ParmDecl)
+    .map(|c| c.get_name())
+    .map(Identifier::from);
+  once(visit_type(defs, ret_ty, Identifier::Return))
+    .chain(
+      arg_tys
+        .into_iter()
+        .zip(arg_names)
+        .map(|(ty, id)| visit_type(defs, ty, id)),
+    )
+    .collect()
 }
 
 fn visit_declaration<'tu>(
@@ -494,7 +562,20 @@ fn visit_declaration<'tu>(
           if pointee_ty.get_kind() == TypeKind::FunctionPrototype
             && is_type_used(e.get_translation_unit(), ty)
           {
-            visit_callback_definition(defs, e, pointee_ty).unwrap();
+            if let Some(ty_maps) =
+              visit_callback_definition(defs, e, pointee_ty)
+            {
+              eprintln!(
+                "{:?}\n{:?}",
+                get_flat_name_for_callback_type(e),
+                ty_maps
+              )
+            } else {
+              eprintln!(
+                "// \x1b[41m{}\x1b[0m",
+                get_flat_name_for_callback_type(e)
+              );
+            }
           }
         }
       }
